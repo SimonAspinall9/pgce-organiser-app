e24ea9f917c6660965a1879bc38488618d9d437e
Simon Aspinall <simon.aspinall@optionis.co.uk>
2018-08-08 15:31:25 +0100

    first comment

---
+++ WebView/WebView/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebView
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		    Browser.Source = "https://pgce-organiser.azurewebsites.net";
+		    NavigationPage.SetHasNavigationBar(this, false);
+		}
+	}
+}
